71585fd7e7e4879500bd414ca100792f1042f7da
pimax <m.pinyugin@gmail.com>
2013-01-23 17:23:06 +0400

    init

--- yii-smsc/SmsC.php
+++ yii-smsc/SmsC.php
@@ -16,116 +16,116 @@ class SmsC extends CApplicationComponent
     public $password;
     
     public function init()
-	{
-		if (!function_exists ('curl_init'))
+    {
+        if (!function_exists ('curl_init'))
         {
             throw new CException ('Для работы расширения требуется cURL');
         }
 
-		parent::init();
-	}
+        parent::init();
+    }
     
     public function send($to, $text, $from = null, $time = null)
     {
         $url = self::HOST . self::SEND;
-		$this->id = null;
+        $this->id = null;
 
-		$params = $this->get_default_params();
-		$params['phones'] = $to;
-		$params['mes'] = $text;
+        $params = $this->get_default_params();
+        $params['phones'] = $to;
+        $params['mes'] = $text;
 
-		if ($from)
-			$params['sender'] = $from;
+        if ($from)
+            $params['sender'] = $from;
 
-		if ($time && $time < (time() + 7 * 60 * 60 * 24))
-			$params['time'] = date("d.m.Y H:i", $time);
-        
+        if ($time && $time < (time() + 7 * 60 * 60 * 24))
+            $params['time'] = date("d.m.Y H:i", $time);
+
         $params['fmt'] = 1;
         $params['cost'] = 3;
 
-		$result = $this->request($url, $params);
-		$result = explode(",", $result);
+        $result = $this->request($url, $params);
+        $result = explode(",", $result);
 
-		return array(
-			'id' => $result[0],
-			'balance' => $result[3]
-		);
+        return array(
+            'id' => $result[0],
+            'balance' => $result[3]
+        );
     }
     
     public function cost($to, $text)
     {
         $url = self::HOST . self::COST;
-		$this->id = null;
+        $this->id = null;
 
-		$params = $this->get_default_params();
-		$params['phones'] = $to;
-		$params['mes'] = $text;
+        $params = $this->get_default_params();
+        $params['phones'] = $to;
+        $params['mes'] = $text;
 
-	
+
         $params['fmt'] = 1;
         $params['cost'] = 1;
 
-		$result = $this->request($url, $params);
-		$result = explode(",", $result);
+        $result = $this->request($url, $params);
+        $result = explode(",", $result);
 
-		return array(
-			'price' => $result[0],
-			'number' => $to
-		);
+        return array(
+            'price' => $result[0],
+            'number' => $to
+        );
     }
     
     public function status($id)
     {
         $url = self::HOST.self::STATUS;
 
-		$params = $this->get_default_params();
-		$params['id'] = $id;
-		$result = $this->request($url, $params);
+        $params = $this->get_default_params();
+        $params['id'] = $id;
+        $result = $this->request($url, $params);
 
-		return $result;
+        return $result;
     }
     
     public function balance()
     {
         $url = self::HOST . self::BALANCE;
 
-		$params = $this->get_default_params();
-		$result = $this->request($url, $params);
+        $params = $this->get_default_params();
+        $result = $this->request($url, $params);
 
-		return $result;
+        return $result;
     }
     
     public function senders() 
     {
-		$url = self::HOST . self::SENDERS;
-		$params = $this->get_default_params();
+        $url = self::HOST . self::SENDERS;
+        $params = $this->get_default_params();
         $params['get_senders'] = 1;
-		$result = $this->request( $url, $params );
-		$result = explode("\n", rtrim($result));
-        
-		return array_map(function ($e) { return substr($e, 7); }, $result);
-	}
+        $result = $this->request( $url, $params );
+        $result = explode("\n", rtrim($result));
+
+        return array_map(function ($e) { return substr($e, 7); }, $result);
+    }
     
     protected function get_default_params() 
     {
-		return array(
+        return array(
             'login' => $this->login,
             'psw' => $this->password
         );
-	}
+    }
     
     protected function request($url, $params = array()) 
     {
-		$ch = curl_init($url);
-		$options = array(
-			CURLOPT_RETURNTRANSFER => 1,
-			CURLOPT_TIMEOUT => 30,
-			CURLOPT_POSTFIELDS => $params
-		);
-		curl_setopt_array($ch, $options);
-		$result = curl_exec($ch);
-		curl_close($ch);
+        $ch = curl_init($url);
+        $options = array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_POSTFIELDS => $params
+        );
+        curl_setopt_array($ch, $options);
+        $result = curl_exec($ch);
+        curl_close($ch);
 
-		return $result;
-	}
+        return $result;
+    }
 }
